02a1dcc051c6165c0626a315923bd09eb60754ec
Alek Russa <ardr12345@gmail.com>
2018-07-11 18:59:40 +0000

    dank meemes

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,19 +1,10 @@
 class TodoController < ApplicationController
     def index
     end
-    
-    
-    def show
-    @todo_desc="Make bank"
-    @todo_time="9999"
-    @todo_due="June 14th"
-
-    
 
 
 
 
 
 
-    end
 end
---
+++ app/models/todo.rb
@@ -0,0 +1,6 @@
+class Todo < ActiveRecord::Base
+    t=Todo.new
+    t.desc="Do Homework"
+    t.time=20
+    
+end
