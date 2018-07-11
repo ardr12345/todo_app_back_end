a006d188b4a46a897d86ad8ee8b1874b8cf3329c
Alek Russa <ardr12345@gmail.com>
2018-07-11 19:40:56 +0000

    dang

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,10 +1,29 @@
 class TodoController < ApplicationController
     def index
     end
+    def show
+        @todo_list_section="france sucks at stuff"
+        @time=5
+        todo_id=params[:id]
+        if todo_id == '1'
+            @todo_list_section="idk"
+            @time=50
+        elsif todo_id == '2'
+            @todo_list_section="gotojapan"
+            @time=23
+        elsif todo_id == '3' 
+            @todo_list_section="gotojn"
+            @time=37
+        elsif todo_id == '4'
+            @todo_list_section="get a life"
+            @time=46
+        elsif todo_id == '5'
+            @todo_list_section="get dog"
+            @time=98
+
+@todo=Todo.find_by_id(params[:id])
 
 
-
-
-
-
+        end
+    end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,2 @@
 class Todo < ActiveRecord::Base
-    t=Todo.new
-    t.desc="Do Homework"
-    t.time=20
-    
 end
