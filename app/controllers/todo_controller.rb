8638325eb900e3e2d65672f6eac6d4f638435577
Alek Russa <ardr12345@gmail.com>
2018-07-11 18:32:46 +0000

    gang gang

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,19 @@
 class TodoController < ApplicationController
     def index
-        
-        
+    end
+    
+    
+    def show
+    @todo_desc="Make bank"
+    @todo_time="9999"
+    @todo_due="June 14th"
+
+    
+
+
+
+
+
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
     get 'todo/index', to:'todo#index'
-  
-
+    get 'todo/AddTask' , to:'todo#AddTask'
 
 end
