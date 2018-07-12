fdd4828955a55c2919ea6d63a34e98d72cb0aeb9
Alek Russa <ardr12345@gmail.com>
2018-07-12 14:58:04 +0000

    Dank

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     
     root to:'todo#index'
+    get '/index', to:'todo#index'
     get '/AddTask' , to:'todo#AddTask'
     get '/create', to:'todo#create'
     get '/destroy', to: 'todo#destroy'
