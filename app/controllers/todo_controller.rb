a4a403e3a9d113f91f64a82c83521e8c5f670695
Alek Russa <ardr12345@gmail.com>
2018-07-12 13:26:46 +0000

    gy

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,29 +4,45 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_list_section="france sucks at stuff"
-        @time=5
-        todo_id=params[:id]
-        @todo=Todo.find_by_id(params[:id])
-        if todo_id == '1'
-            @todo_list_section="idk"
-            @time=50
-        elsif todo_id == '2'
-            @todo_list_section="gotojapan"
-            @time=23
-        elsif todo_id == '3' 
-            @todo_list_section="gotojn"
-            @time=37
-        elsif todo_id == '4'
-            @todo_list_section="get a life"
-            @time=46
-        elsif todo_id == '5'
-            @todo_list_section="get dog"
-            @time=98
-        end
-    end
+          @todo=Todo.find_by_id(params[:id])
+          todo_id = params[:id]
+        
+    #     todo_id=params[:id]
+    #     @todo=Todo.find_by_id(params[:id])
+    #     if todo_id == '1'
+    #         @todo_list_section="idk"
+    #         @time=50
+    #     elsif todo_id == '2'
+    #         @todo_list_section="gotojapan"
+    #         @time=23
+    #     elsif todo_id == '3' 
+    #         @todo_list_section="gotojn"
+    #         @time=37
+    #     elsif todo_id == '4'
+    #         @todo_list_section="get a life"
+    #         @time=46
+    #     elsif todo_id == '5'
+    #         @todo_list_section="get dog"
+    #         @time=98
+    #     end
+    # end
         
     def AddTask
+    end
+    
+    def destroy
+    t = Todo.find_by_id(params[:id])
+    t.destroy
+    redirect_to "/todo/index"
+    end
+    
+    def update
+  t = Todo.find_by_id(params['id'])
+  t.descr = params['desc']
+  t.time = params['time']
+  t.due = params ['due']
+  t.save
+  redirect_to "/todo/AddTask/#{t.id}"
     end
     
     def create
@@ -38,6 +54,6 @@ class TodoController < ApplicationController
     
         
         redirect_to "/todo/index"
-        
+    end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     get 'todo/index', to:'todo#index'
     get 'todo/AddTask' , to:'todo#AddTask'
     get 'todo/create', to:'todo#create'
-    
+    get 'todo/destroy', to: 'todo#destroy'
+    get 'todo/update', to: 'todo#update'
+
 end
